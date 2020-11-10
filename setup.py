ff04f204f906c53830057fa290096a06ced9fde7
kesteraernoudt <kesteraernoudt@yahoo.com>
2020-11-10 18:24:39 +0100

    added setup.cfg and updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -17,6 +17,6 @@ setup(
     author_email='kesteraernoudt@yahoo.com',
     url='https://github.com/kesteraernoudt/pydobiss',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages()
 )
 
