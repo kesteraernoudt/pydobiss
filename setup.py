39e9db43738654fe4a6aab8d770335801dc52856
kesteraernoudt <kesteraernoudt@yahoo.com>
2020-11-10 18:30:51 +0100

    updated setup.py to reflect readmy file type

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     version='0.1.0',
     description='python interface to the dobiss developer api',
     long_description=readme,
+    long_description_content_type="text/markdown",
     author='Kester Aernoudt',
     author_email='kesteraernoudt@yahoo.com',
     url='https://github.com/kesteraernoudt/pydobiss',
